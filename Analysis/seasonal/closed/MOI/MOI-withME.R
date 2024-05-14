608928a922578804f6ad5ee0d72c6357dd1ac58a
qzhan321 <42386885+qzhan321@users.noreply.github.com>
2024-05-14 15:41:22 -0500

    Update MOI-withME.R

--- Analysis/seasonal/closed/MOI/MOI-withME.R
+++ Analysis/seasonal/closed/MOI/MOI-withME.R
@@ -1,9 +1,6 @@
 rm(list=ls())
 suppressPackageStartupMessages({
-  library(ggplot2)
   library(dplyr)
-  library(cowplot)
-  library(stringr)
   library(RSQLite)
 })
 run <- 4
@@ -31,7 +28,7 @@ dir.create(sampledHostsSaveDir3)
 
 readDir <- paste0("/project2/pascualmm/QZ/PhD/projects/intervention/files", run, "/actualRuns/infTablePresence/")
 prefix <- "sim"
-nums <- c(101:109)
+nums <- 101:109
 
 load(paste0("~/others/papersOfficial/FOI/analysis/utils/s_givenMOI_list"))
 p_s_givenMOI <- s_givenMOI_list
@@ -45,72 +42,64 @@ f <- read.table(paste0("~/others/papersOfficial/FOI/analysis/utils/NbVarGenes_MO
 
 a <- min(f$DBLa_upsBC_rep_size)
 b <- max(f$DBLa_upsBC_rep_size)
+
 s_single <- max(45, b)
 
 preIRS <- 200
 IRSDur <- 10
 T_YEAR <- 360
-layers <- c(preIRS - 1, preIRS + 1, preIRS + 2)*T_YEAR  + 300
+layers <- c(preIRS - 1, preIRS + 1)*T_YEAR  + 300
 
 p_microscopy <- 0.57
-nums_w_reps <- NULL
+
 for (i in 1:length(nums)) {
   num <- nums[i]
-  if (num %in% nums_w_reps) {
-    reps <- 0:2
-  } else {
-    reps <- 0
-  }
   sampledHostsSaveDir4 <- paste0(sampledHostsSaveDir3, num, "/")
   dir.create(sampledHostsSaveDir4)
   MOIAll <- NULL
-  for (r in reps) {
-    load(paste0(readDir, seasonality, "/", openness, "/", prefix, "_", num, "_r", r, ".RData"))
-    for (ii in 1:length(layers)) {
-      layer <- layers[ii]
-      infStrain_pre_sub <- infStrain_pre %>% filter(presence == 1, time == layer, is_functional == 1)
-      hostsSub <- sample(unique(infStrain_pre_sub$host_id), round(p_microscopy*length(unique(infStrain_pre_sub$host_id))))
-      microscopyPosHosts <- hostsSub
-      save(microscopyPosHosts, file = paste0(sampledHostsSaveDir4, "rep_", r, "time_", layer, ".RData"))
-      infStrain_pre_sub <- infStrain_pre_sub %>% filter(host_id %in% hostsSub)
-      s_temp_dat <- infStrain_pre_sub %>% group_by(host_id) %>% summarise(n = n_distinct(gene_id))
+  load(paste0(readDir, seasonality, "/", openness, "/", prefix, "_", num, "_r0.RData"))
+  for (ii in 1:length(layers)) {
+    layer <- layers[ii]
+    infStrain_pre_sub <- infStrain_pre %>% filter(presence == 1, time == layer, is_functional == 1)
+    hostsSub <- sample(unique(infStrain_pre_sub$host_id), round(p_microscopy*length(unique(infStrain_pre_sub$host_id))))
+    microscopyPosHosts <- hostsSub
+    # save(microscopyPosHosts, file = paste0(sampledHostsSaveDir4, "rep_0", "time_", layer, ".RData"))
+    infStrain_pre_sub <- infStrain_pre_sub %>% filter(host_id %in% hostsSub)
+    s_temp_dat <- infStrain_pre_sub %>% group_by(host_id) %>% summarise(n = n_distinct(gene_id))
       
-      s_temp <- s_temp_dat$n
-      names <- s_temp_dat$host_id
-      for (j in 1:length(s_temp)) {
-        s <- s_temp[j]
-        if (s >= a & s <= s_single*MOI_max) {
-          numerator <- 0
-          for (b in 1:MOI_max) {
-            if (!is.na(p_s_givenMOI[[as.character(b)]][as.character(s)])) {
-              numerator <- numerator + p_s_givenMOI[[as.character(b)]][as.character(s)]*MOI_priors[as.character(b)]
-            }
+    s_temp <- s_temp_dat$n
+    names <- s_temp_dat$host_id
+    for (j in 1:length(s_temp)) {
+      s <- s_temp[j]
+      if (s >= a & s <= s_single*MOI_max) {
+        numerator <- 0
+        for (b in 1:MOI_max) {
+          if (!is.na(p_s_givenMOI[[as.character(b)]][as.character(s)])) {
+            numerator <- numerator + p_s_givenMOI[[as.character(b)]][as.character(s)]*MOI_priors[as.character(b)]
           }
-          
-          if (numerator == 0) {
-            p_c_givens_all <- rep(0, length(1:MOI_max))
-          } else {
-            p_c_givens_all <- rep(NA, length(1:MOI_max))
-            for (c in 1:MOI_max) {
-              if (!is.na(p_s_givenMOI[[as.character(c)]][as.character(s)])) {
-                temp <- p_s_givenMOI[[as.character(c)]][as.character(s)]*MOI_priors[as.character(c)]/numerator
-                names(temp) <- NULL
-              } else {
-                temp <- 0
-              }
-              p_c_givens_all[c] <- temp
-            }
-          }
-          
-          print(sum(p_c_givens_all))
-          
-          df <- data.frame("p" = max(p_c_givens_all), "MOI" = c(1:MOI_max)[which.max(p_c_givens_all)], "numDBLaTypes" = s, 
-                           "host_id" = names[j], "num" = num, "rep" = r, "time" = layer)
-          MOIAll <- rbind(MOIAll, df)
         }
+          
+        if (numerator == 0) {
+          p_c_givens_all <- rep(0, length(1:MOI_max))
+        } else {
+          p_c_givens_all <- rep(NA, length(1:MOI_max))
+          for (c in 1:MOI_max) {
+            if (!is.na(p_s_givenMOI[[as.character(c)]][as.character(s)])) {
+              temp <- p_s_givenMOI[[as.character(c)]][as.character(s)]*MOI_priors[as.character(c)]/numerator
+              names(temp) <- NULL
+            } else {
+              temp <- 0
+            }
+            p_c_givens_all[c] <- temp
+          }
+        }
+          
+        print(sum(p_c_givens_all))  
+        df <- data.frame("p" = max(p_c_givens_all), "MOI" = c(1:MOI_max)[which.max(p_c_givens_all)], "numDBLaTypes" = s, 
+                         "host_id" = names[j], "num" = num, "time" = layer)
+        MOIAll <- rbind(MOIAll, df)
       }
     }
   }
   save(MOIAll, file = paste0(saveDir3, "MOI_", num, ".RData"))
-  # note that some individuals have their p vector equal to 0, because their number of DBLalpha types falls below 10. Remove them before plotting figures.
 }
