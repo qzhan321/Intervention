1d12e6d1d83b5f7f91e532e4121c5ad5d6ae0621
qzhan321 <42386885+qzhan321@users.noreply.github.com>
2024-05-15 18:21:29 -0400

    Update shannonDiversity.R

--- Analysis/seasonal/closed/diversity/shannonDiversity.R
+++ Analysis/seasonal/closed/diversity/shannonDiversity.R
@@ -1,47 +1,39 @@
 rm(list = ls())
 suppressPackageStartupMessages({
-  library(RSQLite)
-  library(reshape2)
-  library(vegan)
-  library(tidyverse)
-  library(cowplot)
-  library(gridExtra)
-  library(data.table)
+  library(dplyr)
 })
 run <- 4
 wd <- paste0("/project2/pascualmm/QZ/PhD/projects/intervention/files", run, "/actualRuns/infTablePresence/")
 seasonality <- "seasonal"
 openness <- "closed"
 prefix<-"sim"
-nums <- c(101:110)
+nums <- 101:110
 saveDir0 <- paste0("/project2/pascualmm/QZ/PhD/projects/intervention/files", run, "/actualRuns/diversity/shannonDiversity/")
-dir.create(saveDir0)
+if (!dir.exists(saveDir0)) {
+  dir.create(saveDir0)
+}
 saveDir1 <- paste0(saveDir0, seasonality, "/")
-dir.create(saveDir1)
+if (!dir.exists(saveDir1)) {
+  dir.create(saveDir1)
+}
 saveDir2 <- paste0(saveDir1, openness, "/")
-dir.create(saveDir2)
+if (!dir.exists(saveDir2)) {
+  dir.create(saveDir2)
+}
 fName <- "true"
 saveDir3 <- paste0(saveDir2, fName, "/")
-dir.create(saveDir3)
-
-fetchdb<-function(dbname,query,numQuery = 20000000) {
-  r<-dbSendQuery(conn=dbname, query)
-  er<-dbFetch(r,numQuery)
-  while(!dbHasCompleted(r)){
-    er <- rbind(er, dbFetch(r, numQuery))
-    print(nrow(er))
-  }
-  dbClearResult(r)
-  return(er)
+if (!dir.exists(saveDir3)) {
+  dir.create(saveDir3)
 }
-samplingPeriod<-30
+
+samplingPeriod <- 30
 preIRS <- 200
 IRSDur <- 10
 T_YEAR <- 360
 postIRS <- 0
-times <- c(seq(preIRS-3, preIRS+IRSDur+postIRS-1, 1)*T_YEAR+300)
+times <- seq(preIRS - 3, preIRS + IRSDur + postIRS -1, 1)*T_YEAR + 300
 
-nums_w_reps <- c(107:110,120:123)
+nums_w_reps <- 107:110
 for (a in 1:length(nums)) {
   num <- nums[a]
   print(num)
@@ -66,7 +58,6 @@ for (a in 1:length(nums)) {
     shannon2V <- unlist(shannon2L)
     
     df <- data.frame("num" = num, "shannon" = shannon2V, "time" = timesSub, "rep" = r)  
-    
     save(df, file = paste0(saveDir3, "shannonDiversity-", num, "-r", r, ".RData"))
   }
 }
