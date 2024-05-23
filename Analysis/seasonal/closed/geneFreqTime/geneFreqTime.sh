13dcc4e87385286b05470f860db81c05438e88ae
qzhan321 <42386885+qzhan321@users.noreply.github.com>
2024-05-23 08:44:17 -0400

    Update geneFreqTime.sh

--- Analysis/seasonal/closed/geneFreqTime/geneFreqTime.sh
+++ Analysis/seasonal/closed/geneFreqTime/geneFreqTime.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=36:00:00
 #SBATCH --partition=caslake
 #SBATCH --nodes=1
-#SBATCH --array=1-2
+#SBATCH --array=1
 #SBATCH --ntasks-per-node=1 
 #SBATCH --mem-per-cpu=48000
 #SBATCH --account=pi-jozik
