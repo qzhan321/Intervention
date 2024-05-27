149a668df53adf41e64441923a0b3931fdfc5533
qzhan321 <42386885+qzhan321@users.noreply.github.com>
2024-05-27 09:58:17 -0400

    Update Fig3-ExtinctionProbability.R

--- Paper/plots/main/Fig3-ExtinctionProbability.R
+++ Paper/plots/main/Fig3-ExtinctionProbability.R
@@ -20,6 +20,7 @@ T_YEAR <- 360
 IRSType <- "10yIRS"
 reps <- 0:4
 ts <- seq(preIRS + 4, preIRS + 10, 2)
+sizeV <- 31
 for (i in 1:length(seasonality)) {
   s <- seasonality[i]
   saveDir1 <- paste0(saveDir0, s, "/")
@@ -61,7 +62,6 @@ for (i in 1:length(seasonality)) {
     
     dfAll$timeCheck <- factor(dfAll$timeCheck, levels = sort(unique(dfAll$timeCheck)))
     dfAll$IRS <- factor(dfAll$IRS, levels = unique(dfAll$IRS))
-    sizeV <- 31
     p1 <- ggplot(dfAll, aes(IRS, p_extinc, alpha = timeCheck, col = IRS, fill = IRS))+ 
       geom_bar(stat = "identity", position = "dodge")+
       xlab("IRS Runs") + ylab("Probability of Extinction") +
@@ -69,16 +69,11 @@ for (i in 1:length(seasonality)) {
         plot.title = element_text(color="black", size=sizeV, hjust = 0.5),
         axis.title.x = element_text(color="black", size=sizeV),
         axis.title.y = element_text(color="black", size=sizeV),
-        axis.text.x = element_text(color="black", 
-                                   size=sizeV, angle=0),
-        axis.text.y = element_text(color="black", 
-                                   size=sizeV, angle=0),
-        legend.text = element_text(color="black", 
-                                   size=sizeV, angle=0),
-        legend.title = element_text(color="black", 
-                                    size=sizeV, angle=0),
-        strip.text = element_text(color="black", 
-                                  size=sizeV, angle=0)) +
+        axis.text.x = element_text(color="black", size=sizeV, angle=0),
+        axis.text.y = element_text(color="black", size=sizeV, angle=0),
+        legend.text = element_text(color="black", size=sizeV, angle=0),
+        legend.title = element_text(color="black", size=sizeV, angle=0),
+        strip.text = element_text(color="black", size=sizeV, angle=0)) +
       scale_color_manual(values = Turbo(out.colors = 17)[((min(nums)-200) + 1):((max(nums)-200) + 1)]) +
       scale_fill_manual(values = Turbo(out.colors = 17)[((min(nums)-200) + 1):((max(nums)-200) + 1)]) + 
       guides(fill = "none", col = "none") +
