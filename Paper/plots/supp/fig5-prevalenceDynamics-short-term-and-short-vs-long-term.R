4db258f53297885e45fdae5a6a324e3698fe2af0
qzhan321 <42386885+qzhan321@users.noreply.github.com>
2024-05-30 12:29:23 -0400

    Create fig5-prevalenceDynamics-short-term-and-short-vs-long-term.R

---
+++ Paper/plots/supp/fig5-prevalenceDynamics-short-term-and-short-vs-long-term.R
@@ -0,0 +1,219 @@
+rm(list = ls())
+suppressPackageStartupMessages({
+  library(ggplot2)
+  library(dplyr)
+  library(ggpubr)
+})
+run <- 4
+source("/home/qizhan/others/PhD/projects/intervention/writings/simulation4/plots/colorFunc.R")
+saveDir0 <- "/project2/pascualmm/QZ/PhD/projects/intervention/natComRevision/plots/figures/supp/fig5/prevalenceDynamicsShortTerm/"
+if (!dir.exists(saveDir0)) {
+  dir.create(saveDir0)
+}
+saveDir1 <- "/project2/pascualmm/QZ/PhD/projects/intervention/natComRevision/plots/figures/supp/fig5/prevalenceDynamicsShortTermVsLongTerm/"
+if (!dir.exists(saveDir1)) {
+  dir.create(saveDir1)
+}
+readDir <- paste0("/project2/pascualmm/QZ/PhD/projects/intervention/files", run, "/actualRuns/epi/")
+numsPlotList <- list(
+  "seasonal closed" = list(101:110),
+  "seasonal semi-open" = list(201:211, 301:311),
+  "seasonal regionally-open" = list(219:229, 237:245, 255:265),
+  "non-seasonal closed" = list(101:111),
+  "non-seasonal semi-open" = list(201:211, 301:311),
+  "non-seasonal regionally-open" = list(201:210,219:228, 237:246,255:264)
+)
+labelsList <- list(
+  "seasonal closed" = list("Seasonal \nClosed \n \n"),
+  "seasonal semi-open" = list("Seasonal \nSemi-open \nBaseline Migration \n", 
+                              "Seasonal \nSemi-open \nHigh Migration \n"),
+  "seasonal regionally-open" = list("Seasonal \nRegionally-open \nBaseline Migration \nLarge Pool",
+                                    "Seasonal \nRegionally-open \nHigh Migration \nMedium Pool",
+                                    "Seasonal \nRegionally-open \nHigh Migration \nLarge Pool"),
+  "non-seasonal closed" = list("Non-seasonal \nClosed \n \n"),
+  "non-seasonal semi-open" = list("Non-seasonal \nSemi-open \nBaseline Migration \n",  
+                                  "Non-seasonal \nSemi-open \nHigh Migration \n"),
+  "non-seasonal regionally-open" = list("Non-seasonal \nRegionally-open \nBaseline Migration \nMedium Pool",
+                                        "Non-seasonal \nRegionally-open \nBaseline Migration \nLarge Pool",
+                                        "Non-seasonal \nRegionally-open \nHigh Migration \nMedium Pool",
+                                        "Non-seasonal \nRegionally-open \nHigh Migration \nLarge Pool")
+)
+immsList <- list(
+  "seasonal closed" = list("Three-year"),
+  "seasonal semi-open" = list("Three-year", "Three-year"),
+  "seasonal regionally-open" = list("Three-year", "Three-year", "Three-year"),
+  "non-seasonal closed" = list("Three-year"),
+  "non-seasonal semi-open" = list("Three-year", "Three-year"),
+  "non-seasonal regionally-open" = list("Three-year", "Three-year", "Three-year", "Three-year")
+)
+T_YEAR <- 360
+IRSDur <- 10
+postIRS <- 10 - IRSDur
+IRSType <- paste0(IRSDur, "yIRS")
+seasonality <- c("seasonal", "non-seasonal")
+openness <- c("closed", "semi-open", "regionally-open")
+preIRSList <- list(
+  "seasonal closed" = 200,
+  "seasonal semi-open" = 150, 
+  "seasonal regionally-open" = 150,
+  "non-seasonal closed" = 200,
+  "non-seasonal semi-open" = 150,
+  "non-seasonal regionally-open" = 150
+)
+sizeV <- 30
+pointSize <- 2.75
+for (i in 1:length(seasonality)) {
+  s <- seasonality[i]
+  saveDir2 <- paste0(saveDir0, s, "/")
+  if (!dir.exists(saveDir2)) {
+    dir.create(saveDir2)
+  }
+  saveDir3 <- paste0(saveDir1, s, "/")
+  if (!dir.exists(saveDir3)) {
+    dir.create(saveDir3)
+  }
+  
+  for (j in 1:length(openness)) {
+    o <- openness[j]
+    saveDir4 <- paste0(saveDir2, o, "/")
+    if (!dir.exists(saveDir4)) {
+      dir.create(saveDir4)
+    }
+    saveDir5 <- paste0(saveDir3, o, "/")
+    if (!dir.exists(saveDir5)) {
+      dir.create(saveDir5)
+    }
+    
+    file <- paste0(readDir, s, "/", o, "/", "summaryInfoTable-", IRSType, ".RData")
+    load(file)
+    preIRS <- preIRSList[[paste0(s, " ", o)]]
+    numsPlot <- numsPlotList[[paste0(s, " ", o)]]
+    labels <- labelsList[[paste0(s, " ", o)]]
+    imms <- immsList[[paste0(s, " ", o)]]
+    for (k in 1:length(numsPlot)) {
+      nums <- numsPlot[[k]]
+      label <- labels[[k]]
+      imm <- imms[[k]]
+      
+      saveDir6 <- paste0(saveDir4, imm, "/")
+      if (!dir.exists(saveDir6)) {
+        dir.create(saveDir6)
+      }
+      saveDir7 <- paste0(saveDir5, imm, "/")
+      if (!dir.exists(saveDir7)) {
+        dir.create(saveDir7)
+      }
+      
+      summaryTable1 <- summaryTableCombined %>% filter(num %in% nums)
+      summaryTable2 <- summaryTable1 %>% filter(time > (preIRS-3)*T_YEAR, time <= (preIRS + IRSDur + postIRS)*T_YEAR)
+      summaryTable3 <- summaryTable2 %>% group_by(time, IRS) %>%
+        summarise(mP = mean(Prevalence), vP = sd(Prevalence), 
+                  maxP = max(Prevalence), minP = min(Prevalence),
+                  mD = mean(n_circulating_genes), vD = sd(n_circulating_genes),
+                  maxD = max(n_circulating_genes), minD = min(n_circulating_genes),
+                  mMOI = mean(MOI, na.rm = T), vMOI = sd(MOI, na.rm = T))
+      summaryTable4 <- summaryTable3 %>% mutate(vP = ifelse(is.na(vP), 0, vP), 
+                                                vD = ifelse(is.na(vD), 0, vD),
+                                                vMOI = ifelse(is.na(vMOI), 0, vMOI))
+      summaryTable4$IRS <- factor(summaryTable4$IRS, levels = c("preIRS", paste0("I-", 1:16)))
+      
+      if (s == "seasonal") {
+        t1 <- (preIRS + 1)*T_YEAR + 300
+        t2 <- (preIRS + 9)*T_YEAR + 300
+      } else {
+        t1 <- (preIRS + 2)*T_YEAR 
+        t2 <- (preIRS + 10)*T_YEAR 
+      }
+      df1 <- summaryTable4 %>% filter(time == t1) %>% ungroup() %>%
+        select(IRS, mP, minP, maxP)
+      colnames(df1)[2:4] <- paste0("short_term_rebound_", colnames(df1)[2:4])
+      df2 <- summaryTable4 %>% filter(time == t2) %>% ungroup() %>%
+        select(IRS, mP, minP, maxP)
+      colnames(df2)[2:4] <- paste0("longer_term_rebound_", colnames(df2)[2:4])
+      
+      df12 <- left_join(df1, df2, by = c("IRS"))
+      
+      if (s == "seasonal" & o == "closed") {
+        ytitleC <- "black"
+        ytextC <- "black"
+      } else if (s == "seasonal" & o == "regionally-open" & identical(nums, 255:265)) {
+        ytitleC <- "black"
+        ytextC <- "black"
+      } else if (s == "non-seasonal" & o == "regionally-open" & identical(nums, 219:228)) {
+        ytitleC <- "black"
+        ytextC <- "black"
+      } else {
+        ytitleC <- "white"
+        ytextC <- "white"
+      }
+      if (s == "seasonal" & o == "semi-open" & identical(nums, 301:311)) {
+        xtitleC <- "black"
+      } else if (s == "non-seasonal" & o == "semi-open" & identical(nums, 201:211)) {
+        xtitleC <- "black"
+      } else if (s == "non-seasonal" & o == "regionally-open" & identical(nums, 255:264)) {
+        xtitleC <- "black"
+      } else {
+        xtitleC <- "white"
+      }
+      p1 <- ggplot(df12, aes(x = short_term_rebound_mP, y = longer_term_rebound_mP, col = IRS, fill = IRS))+ 
+        geom_point(size = pointSize) +
+        geom_errorbarh(aes(xmin = short_term_rebound_minP, xmax = short_term_rebound_maxP), height = pointSize/100, size = pointSize/4) +
+        geom_errorbar(aes(ymin = longer_term_rebound_minP, ymax = longer_term_rebound_maxP), width = pointSize/100, size = pointSize/4) +
+        ggtitle(label) +
+        xlab("Prevalence \n(Short-term Rebound)") + ylab("Prevalence \n(Long-term Rebound)") +
+        theme_bw() + theme(
+          plot.title = element_text(color="black", size=sizeV/1.25, hjust = 0.5),
+          axis.title.x = element_text(color=xtitleC, size=sizeV),
+          axis.title.y = element_text(color=ytitleC, size=sizeV),
+          axis.text.x = element_text(color="black", size=sizeV/1.25, angle=0),
+          axis.text.y = element_text(color=ytextC, size=sizeV, angle=0),
+          legend.text = element_text(color="black", size=sizeV, angle=0),
+          legend.title = element_text(color="black", size=sizeV, angle=0),
+          strip.text = element_text(color="black", size=sizeV, angle=0)) +
+        scale_color_manual(values = Turbo(out.colors = 17)[2:(length(nums) + 1)]) +
+        scale_fill_manual(values = Turbo(out.colors = 17)[2:(length(nums) + 1)]) +
+        coord_cartesian(xlim = c(0,1), ylim = c(0,1)) + 
+        guides(fill = "none", col = "none") +
+        scale_x_continuous(expand = expansion(mult = 0.1)) + 
+        scale_y_continuous(expand = expansion(mult = 0.1))
+      print(p1)
+      
+      forwardYs <- 1
+      if (s == "seasonal") {
+        df1 <- summaryTable4 %>% filter(time == (preIRS + forwardYs)*T_YEAR + 300)
+      } else {
+        df1 <- summaryTable4 %>% filter(time == (preIRS + forwardYs + 1)*T_YEAR)
+      }
+      summaryTable2 <- summaryTable1 %>% filter(!(time == preIRS * T_YEAR & IRS %in% c(paste0("I-", 1:16))))
+      CRs <- summaryTable2 %>% select(IRS, CR) %>% group_by(IRS) %>% summarise(meanCR = mean(CR))
+      df2 <- df1 %>% left_join(CRs, by = "IRS")
+      df2$IRS <- factor(df2$IRS, levels = paste0("I-", 1:16))
+      epiCR <- ggplot(df2, aes(x = meanCR, y = mP, col = IRS)) + 
+        geom_point(size = pointSize) + 
+        geom_errorbar(aes(ymin = minP, ymax = maxP), width = pointSize/3, size = pointSize/4) +
+        theme_bw() +
+        ggtitle(label) +
+        xlab("Effective Contact Rate \n(per Host, per Year)") + 
+        ylab("Prevalence \n(Short-term Rebound)") +
+        theme(
+          plot.title = element_text(color="black", size = sizeV/1.25, hjust = 0.5, vjust = 1),
+          axis.title.x = element_text(color=xtitleC, size = sizeV),
+          axis.title.y = element_text(color=ytitleC, size = sizeV),
+          axis.text.x = element_text(color="black", size = sizeV/1.25, angle=0),
+          axis.text.y = element_text(color=ytextC, size = sizeV, angle=0),
+          legend.text = element_text(color="black", size = sizeV, angle=0),
+          legend.title = element_blank(),
+          strip.text = element_text(color="black", size = sizeV, angle=0),
+          plot.margin = unit(c(5,22,5,5), "points"),
+          legend.position = c(0.9, 0.04)) + 
+        scale_color_manual(values = Turbo(out.colors = 17)[2:(length(nums) + 1)]) +
+        scale_fill_manual(values = Turbo(out.colors = 17)[2:(length(nums) + 1)]) +
+        guides(col = "none", fill = "none") +
+        coord_cartesian(ylim = c(0, 1))
+      epiCR
+      ggsave(paste0(saveDir6, "num-", min(nums), "-", max(nums), "-cr.pdf"), epiCR, width = 6.45, height = 7.45)
+      ggsave(paste0(saveDir7, "num-", min(nums), "-", max(nums), ".pdf"), p1, width = 6.45, height = 7.45)
+    }
+  }
+}
+
