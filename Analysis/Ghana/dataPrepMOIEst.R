f49d4d5beecafc0cb29e938a565f6755dbd4b655
qzhan321 <42386885+qzhan321@users.noreply.github.com>
2024-06-13 08:19:00 -0400

    Update dataPrepMOIEst.R

--- Analysis/Ghana/dataPrepMOIEst.R
+++ Analysis/Ghana/dataPrepMOIEst.R
@@ -7,21 +7,24 @@ suppressPackageStartupMessages({
   library(dplyr)
 })
 
-saveDir <- "/project2/pascualmm/QZ/PhD/projects/intervention/natComRevision/files/figures/main/Fig5/MOI/MOIEstInputs/"
+saveDir <- "/project2/pascualmm/QZ/PhD/projects/intervention/natComRevision/files/figures/main/Fig5-290923_NYU/MOI/MOIEstInputs/"
 if (!dir.exists(saveDir)) {
   dir.create(saveDir)
 }
 
-readDir <- "/project2/pascualmm/QZ/PhD/projects/FOI/files/Ghana/surveys/BC/"
-surveys  <- c("survey_1", "survey_4", "survey_5")
-files <- paste0(surveys, ".csv")
+readDir <- "/project2/pascualmm/QZ/PhD/projects/intervention/natComRevision/files/figures/main/Fig5-290923_NYU/GhanaIndividualSurvey/UpsBC/"
+prefix <- "survey"
+nums  <- c(1,4,5)
+files <- paste0(prefix, "_", nums, ".csv")
 
 for (i in 1:length(files)) {
   file <- files[i]
-  survey <- surveys[i]
+  num <- nums[i]
   dataT <- read.csv(paste0(readDir, file), header = T, row.names = 1)
+  print(dim(dataT))
+  table(dataT > 1)
   isolateSizes <- rowSums(dataT > 0)
   MOIInput <- data.frame("HostID" = names(isolateSizes), "DBLa_upsBC_rep_size" = unname(isolateSizes))
-  write.csv(MOIInput, file = paste0(saveDir, survey, ".csv"), row.names = F)
+  write.csv(MOIInput, file = paste0(saveDir, prefix, "_", num, ".csv"), row.names = F)
 }
 
