870c8f01b8054c17f7b3ef409ef1777f00b38887
qzhan321 <42386885+qzhan321@users.noreply.github.com>
2024-06-21 19:40:34 -0400

    Update fig4-infectionDur.R

--- Paper/plots/supp/fig4-infectionDur.R
+++ Paper/plots/supp/fig4-infectionDur.R
@@ -5,7 +5,6 @@ suppressPackageStartupMessages({
   library(ggpubr)
 })
 run <- 4
-source(paste0("/home/qizhan/others/PhD/projects/intervention/writings/simulation", run, "/plots/colorFunc.R"))
 readDir <- paste0("/project2/pascualmm/QZ/PhD/projects/intervention/files", run, "/actualRuns/infectionDur/")
 openness <- c("closed", "semi-open", "regionally-open")
 seasonality <- c("seasonal", "non-seasonal")
